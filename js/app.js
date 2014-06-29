1c3359a28399e897d1011ed8b746c27594753b14
ederyair <ederyairnglz@gmail.com>
2014-06-29 14:56:02 -0500

    Now you get the video with lyrics
    Add the new feature of getting the video with the lyrics, using the
    YouTube API, I’m using the YouTube API because I was not able to find a
    good API provider of full lyrics

--- js/app.js
+++ js/app.js
@@ -1,50 +1,82 @@
   var dropZone = document.getElementById('drop_section');
-  dropZone.addEventListener('dragover', DragOver, false);
-  dropZone.addEventListener('drop', FileSelect, false);
-  $('#recent_drags').on('click', muestra);
-  $('#email').on('click', show);
-  
-  function DragOver(evt) {
+  //YouTube API variables
+  var url = 'http://gdata.youtube.com/feeds/api/videos';
+  var result = $('#vidlyrics');
+function start (template) {
+  $('#recent_info').prepend(template);
+}
+function DragOver(evt) {
     evt.stopPropagation();
     evt.preventDefault();
     $('#drop_section').addClass('dragover');
-  }
-  function FileSelect(evt) {
+}
+function FileSelect(evt) {
     evt.stopPropagation();
     evt.preventDefault();
-
     var files = evt.dataTransfer.files; 
-   
-    console.log(files)
     for (var i = 0,f; f = files[i]; i++) {
-      var nombre_cancion = f.name;
-       console.log(f)
+      var nombre_cancion;
+      nombre_cancion = f.name;
     }
-      nombre_cancion = nombre_cancion.replace (/.mp3/g, "");
-      nombre_cancion = nombre_cancion.replace (/.m4a/g, "");
-      
-      var template = '<div class="song" id="song">\
+      nombre_cancion =  nombre_cancion.replace (/.mp3/g, "");
+      nombre_cancion =  nombre_cancion.replace (/.m4a/g, "");
+      var query = nombre_cancion;
+      var template= '<div class="song" id="song">\
                           <figure class="img_recent margin_top">\
                                  <img src="images/recent.jpg" alt="">\
                           </figure> \
-                          <p>' +nombre_cancion+'</p>\
+                          <p>' + nombre_cancion+'</p>\
                       </div>';
-     $('.titulo').append(nombre_cancion);
-     $('#recent_info').prepend(template)
+    $('#titulo').prepend(nombre_cancion);
     $('#drop_section').removeClass('dragover');
-  } 
- 
-  function muestra () {
+    start(template);
+    submit(query);
+} 
+//YouTube API
+function submit(query) {
+    $.ajax({
+        data : {
+            alt: 'json',
+            q: query,
+            'max-results': 1
+        },
+        url: url
+    }).done( callback );
+}
+function videoTemplate(video) {
+    var embed = video.media$group.media$content[0].url.split('?')[0].replace('/v/','/embed/')
+    var html = "";
+    html += '<article>';
+    html += ' <div><iframe src="' + embed + '" frameborder="0" allowfullscreen></iframe></div></article>';
+    return html;
+}
+function callback(res) {
+    res = res.feed.entry;
+    var html = '';
+    for (var i=0; i<res.length; i++) {
+        html += videoTemplate(res[i]);
+    }
+    result.html(html);
+}
+//Animations
+function muestra () {
     var song_length = $('#song').length;
     if(song_length == 0){
       alert('No haz drageado ninguna canción aun')
     }else{
       $('#recent_info').slideToggle();
     }
-  }
-  function show () {
+}
+function show () {
     var css={
-      opacity: 1
+      opacity: 0.8,
+      transition: '1s all ease'
     };
-    $('.mail').css(css)
-  }
+    $('#mail').css(css)
+}
+
+$(document).on('ready',start);
+$('#recent_drags').on('click', muestra);
+$('#email').on('click', show);
+dropZone.addEventListener('dragover', DragOver, false);
+dropZone.addEventListener('drop', FileSelect,false);
